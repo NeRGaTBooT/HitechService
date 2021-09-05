d9d5ce39f01708f97a8ca1ee4db888173898bc8a
Galchinsky Oleg <galchinsky.oleg@gmail.con>
2021-09-05 10:39:55 +0300

    init project

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
